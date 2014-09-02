d3d1bdd008c9f85fae1332a2b532b3c21d45a63c
Dave Syer <dsyer@pivotal.io>
2014-09-02 19:36:20 +0100

    Make hystrix dashboard embeddable finally

--- src/main/java/hystrixdashboard/HystrixDashboardApplication.java
+++ src/main/java/hystrixdashboard/HystrixDashboardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.hystrix.annotations.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;

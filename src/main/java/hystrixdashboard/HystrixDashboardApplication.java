2565807a008fb9123a1606f8ea8ef7b9fd1d38ed
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2021-01-13 16:24:50 +0100

    Upgrade to Boot 2.3.7.RELEASE and Cloud Hoxton.SR9. Upgrade maven and gradle wrappers.

--- src/main/java/hystrixdashboard/HystrixDashboardApplication.java
+++ src/main/java/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,44 +2,34 @@ package hystrixdashboard;
 
 import hystrixdashboard.stream.MockStreamServlet;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by sgibb on 7/11/14.
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @Controller
 @EnableHystrixDashboard
-public class HystrixDashboardApplication extends SpringBootServletInitializer {
-	
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
 	@RequestMapping("/")
 	public String home() {
 		return "forward:/hystrix";
 	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HystrixDashboardApplication.class).web(true);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
-    }
-
-    @Bean
-    public ServletRegistrationBean mockStreamServlet() {
-        return new ServletRegistrationBean(new MockStreamServlet(), "/mock.stream");
-    }
+	@Bean
+	public ServletRegistrationBean mockStreamServlet() {
+		return new ServletRegistrationBean(new MockStreamServlet(), "/mock.stream");
+	}
 
 }
